8fe365c6cfa4842b04866a9eb2908ac609e5d8bc
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-06-09 18:53:44 -0300

    new features

--- chella/src/pages/Home/index.jsx
+++ chella/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import AboutImg from "../../assets/Home/Imagemhomepage1.png";
+import ingressoIcon from "../../assets/Home/íconeIngresso.png";
 import "./style.css";
 
 export default function Home() {
@@ -7,9 +8,10 @@ export default function Home() {
       <div className="container-banner">
         <h1>Boas-vindas ao #CodeChella2023!</h1>
       </div>
-      <section className="container">
+      <section className="container-about">
         <div className="container-content">
-          <img src={AboutImg} alt="imagem" />
+          <img className="about-img" src={AboutImg} alt="imagem" />
+
           <div>
             <h6>11 e 12 de Março Aluródromo de São Paulo</h6>
             <p>
@@ -18,6 +20,37 @@ export default function Home() {
               um festival diverso, com vários artistas e referências.
               Divirta-se!
             </p>
+            <div className="container-button">
+              <button>
+                Comprar ingresso! <img src={ingressoIcon} alt="iIcon" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container-line-up-d1">
+        <div className="container-lineUp">
+          <h1>/Line-Up/</h1>
+        </div>
+        <div className="container-data1">
+          <div className="day1">
+            <h3>SÁBADO 11/03</h3>
+          </div>
+        </div>
+        <div className="container-musics">
+          <div className="container-music-title">
+            <h2>System of a DOM</h2>
+          </div>
+          <div className="musics">
+            <div>
+              <h5>Python Maiden</h5>
+              <h5>Python Maiden</h5>
+              <h5>Python Maiden</h5>
+              <h5>Python Maiden</h5>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
       </section>
