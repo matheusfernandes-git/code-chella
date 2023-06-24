5b68d902c7fc8367a46d1a22f1957f5367cbaf8c
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-06-23 23:29:44 -0300

    .

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Footer from "./components/Footer";
 import Setores from "./pages/Sectors";
 import Informations from "./pages/Informations";
 import Form from "./pages/Form/index";
-import Ticket from 'pages/Ticket/index';
+import Ticket from "pages/Ticket/index";
 import { useState } from "react";
 
 export default function App() {
   const [list, setList] = useState([]);
 
-  function handleClient(client){
-    let newList = [...list]
+  function handleClient(client) {
+    let newList = [...list];
     newList.push(client);
     setList(newList);
   }
@@ -26,7 +26,10 @@ export default function App() {
         <Route path="/experiencia" element={<Experience />} />
         <Route path="/setores" element={<Setores />} />
         <Route path="/informacoes" element={<Informations />} />
-        <Route path="/formulario" element={<Form onAddClient={handleClient} />} />
+        <Route
+          path="/formulario"
+          element={<Form onAddClient={handleClient} />}
+        />
         <Route path="/ingresso" element={<Ticket list={list} />} />
       </Routes>
       <Footer />
