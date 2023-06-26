2872a47d54f39c3baab50ed2f20cb897c3b539ff
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-06-26 19:05:09 -0300

    Update index.jsx

--- src/pages/Ticket/InfoList/index.jsx
+++ src/pages/Ticket/InfoList/index.jsx
@@ -2,12 +2,12 @@ export default function InfoList({ name, ticket }) {
   return (
     <>
       <h6>{name}</h6>
-        <ul>
-          <li>{ticket}</li>
-          <li>Setor Pista</li>
-          <li>Data: 11/03</li>
-          <li>Local: São Paulo-SP</li>
-        </ul>
+      <ul>
+        <li>{ticket}</li>
+        <li>Setor Pista</li>
+        <li>Data: 11/03</li>
+        <li>Local: São Paulo-SP</li>
+      </ul>
     </>
   );
 }
